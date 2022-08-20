9a85df65f49d192040ade2992ed0f92abe98f473
Ken <103093259+ken862734801@users.noreply.github.com>
2022-08-20 15:56:50 -0400

    added function to update read status
    user can update read status, and code was refactored

--- script.js
+++ script.js
@@ -6,25 +6,7 @@ const form = document.querySelector("form");
 const title = document.getElementById("bookTitle");
 const author = document.getElementById("bookAuthor");
 const pages = document.getElementById("bookPages");
-const checkbox = document.getElementById("bookStatus");
-
-//Check the read status.
-let isChecked;
-let readStatus;
-
-function getReadStatus(){
-    if(this.checked){
-        isChecked = true;
-        readStatus = "Read";
-        console.log("Read");
-    }else if(!this.checked){
-        isChecked = false;
-        readStatus = "Not Read";
-        console.log("Not Read");
-    }
-};
-
-checkbox.addEventListener("click", getReadStatus);
+const readStatus = document.getElementById("bookStatus");
 
 //Library array.
 myLibrary = [];
@@ -35,151 +17,169 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.info = function(){
+        return `${title} by ${author}, ${pages} pages, Read: ${read}.`;
+    }
+};
+
+function addNewBookToLibrary(){
+    let newBook = new Book(title.value, author.value, pages.value, readStatus.checked);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 };
 
 //Example book information. 
 const defaultBook = new Book(
     "Harry Potter and the Goblet of Fire",
-    "J.K. Rowling", "636", "Read"
+    "J.K. Rowling", "636", true
 );
 const defaultBook2 = new Book(
     "Of Mice and Men",
-    "John Steinbeck", "107", "Not Read"
+    "John Steinbeck", "107", false
 )
 const defaultBook3 = new Book(
     "The Outsiders",
-    "S.E. Hinton", "192", "Read"
+    "S.E. Hinton", "192", true
 )
 
-//Add example books to library. 
 function addDefaultBooksToLibrary(){
-    myLibrary.push(defaultBook);
-    myLibrary.push(defaultBook2);
-    myLibrary.push(defaultBook3);
+    myLibrary.push(defaultBook, defaultBook2, defaultBook3);
     console.log(myLibrary);
-}
-
-//Function to add new book from input. 
-function addNewBookToLibrary(){
-    const read = readStatus;
-    newBook = new Book(title.value, author.value, pages.value, read);
-
-    myLibrary.push(newBook);
-    console.log(myLibrary);
-    hideModal();
+    createBook();
 };
 
-//Function to create book cards.
-function createBooks(){
-  
+//Add information to array and create example cards on load.
+document.addEventListener("DOMContentLoaded", addDefaultBooksToLibrary);
+
+function createBook(){
+
     container.textContent="";
 
-    for(let i= 0; i < myLibrary.length; i++){
+    for(i=0; i < myLibrary.length; i++){
 
-    let cardDiv = document.createElement("div");
-    let headerDiv = document.createElement("div");
-    let span = document.createElement("span");
-    let statusDiv = document.createElement("div");
-    let titleDiv = document.createElement("div");
-    let authorDiv = document.createElement("div");
-    let pagesDiv = document.createElement("div");
-    let buttonDiv = document.createElement("div");
-    let label = document.createElement("label");
-    let p = document.createElement("p");
-    let checkbox2 = document.createElement("input");
-    let span2 = document.createElement("span");
+        //Create DOM elements.
+        let cardDiv = document.createElement("div");
+        let headerDiv = document.createElement("div");
+        let span = document.createElement("span");
+        let statusDiv = document.createElement("div");
+        let titleDiv = document.createElement("div");
+        let authorDiv = document.createElement("div");
+        let pagesDiv = document.createElement("div");
+        let buttonDiv = document.createElement("div");
+        let p = document.createElement("p");
+        let label = document.createElement("label");
+        let checkbox = document.createElement("input");
+        let span2 = document.createElement("span");
 
-    cardDiv.className = "card";
-    cardDiv.id = `${myLibrary[i].title}`;
-    headerDiv.className = "card-header";
-    span.className = "remove";
-    span.id = "remove-"+[i];
-    titleDiv.className = "card-title";
-    statusDiv.className = "card-status";
-    authorDiv.className = "card-author";
-    buttonDiv.className = "card-button";
-    label.className = "switch"
-    span2.className = "slider round";
-   
-    checkbox2.type = "checkbox";
+        //DOM element details.
+        cardDiv.className = "card";
+        cardDiv.id = `${myLibrary[i].title}`
+        cardDiv.setAttribute("data-index", `${i}`);
+        headerDiv.className = "card-header";
+        span.className = "remove";
+        titleDiv.className = "card-title";
+        statusDiv.className = "card-status";
+        authorDiv.className = "card-author";
+        pagesDiv.className = "card-pages";
+        buttonDiv.className = "card-button";
+        label.className = "switch";
+        checkbox.type = "checkbox";
+        checkbox.className = "toggleBtn";
+        span2.className = "slider round";
 
-    container.appendChild(cardDiv);
-    cardDiv.appendChild(headerDiv);
-    headerDiv.appendChild(span);
-    cardDiv.appendChild(statusDiv);
-    cardDiv.appendChild(titleDiv);
-    cardDiv.appendChild(authorDiv);
-    cardDiv.appendChild(pagesDiv);
-    cardDiv.appendChild(buttonDiv);
-    buttonDiv.appendChild(p);
-    buttonDiv.appendChild(label);
-    label.appendChild(checkbox2);
-    label.appendChild(span2);
+        //Append DOM elements.
+        container.appendChild(cardDiv);
+        cardDiv.appendChild(headerDiv);
+        headerDiv.appendChild(span);
+        cardDiv.appendChild(statusDiv);
+        cardDiv.appendChild(titleDiv);
+        cardDiv.appendChild(authorDiv);
+        cardDiv.appendChild(pagesDiv);
+        cardDiv.appendChild(buttonDiv);
+        buttonDiv.appendChild(p);
+        buttonDiv.appendChild(label);
+        label.appendChild(checkbox);
+        label.appendChild(span2);
 
-    span.textContent = "\u00D7"
-    titleDiv.textContent = `${myLibrary[i].title}`;
-    authorDiv.textContent = `Written by ${myLibrary[i].author}`;
-    pagesDiv.textContent = `${myLibrary[i].pages} pages`;
-    p.textContent = "Read:"
+        //DOM text elements.
+        span.textContent = "\u00D7";
+        titleDiv.textContent = `${myLibrary[i].title}`;
+        authorDiv.textContent = `Written by ${myLibrary[i].author}`;
+        pagesDiv.textContent = `${myLibrary[i].pages} pages`;
+        p.textContent = "Read:";
 
-        if (myLibrary[i].read === "Read"){
+        //DOM Elements related to read status.
+        if(myLibrary[i].read === true){
             statusDiv.classList.add("complete")
             statusDiv.textContent = "Finished";
-            checkbox2.checked = true;
-        }else if (myLibrary[i].read === "Not Read"){
+            checkbox.checked = true;
+        }else if(myLibrary[i].read === false){
             statusDiv.textContent = "Not Finished";
-            statusDiv.classList.add("not-complete")
-            checkbox2.checked = false;
-        }
-    };
-
-    deleteBookCard();
+            statusDiv.classList.add("not-complete");
+            checkbox.checked = false;
+        };
+    }
+    updateBook();
+    deleteBook();
 };
 
-function deleteBookCard(){
+//Function to update book card.
+function updateBook(){
+    const toggleBtn = document.querySelectorAll(".toggleBtn");
+
+    for(i=0; i < toggleBtn.length; i++){
+        toggleBtn[i].addEventListener("change", function(e){
+            let bookToUpdate = e.target.closest("div.card");
+            let bookId = bookToUpdate.id;
+            const bookItem = myLibrary.findIndex(item => item.title === bookId);
+            if(myLibrary[bookItem].read === true){
+                myLibrary[bookItem].read = false;
+                console.log(myLibrary);
+                createBook();
+            }else{
+                myLibrary[bookItem].read = true;
+                createBook();
+                console.log(myLibrary);
+            }
+            
+        });
+    };
+}
+
+//Function to delete book card.
+function deleteBook(){
     const remove = document.querySelectorAll(".remove");
-    console.log(remove.length);
 
     for(i=0; i < remove.length; i++){
         remove[i].addEventListener("click", function(e){
             e.preventDefault();
-            let bookToDelete = e.target.closest("div.card")
+            let bookToDelete = e.target.closest("div.card");
             bookToDelete.remove();
-            console.log(bookToDelete);
 
             let bookId = bookToDelete.id;
-            console.log(bookId);
-
             const bookItem = myLibrary.findIndex(item => item.title === bookId);
             myLibrary.splice(bookItem, 1);
 
-            console.log(myLibrary.length);
-            console.log(myLibrary);
-
             displayMessage();
         });
-    }
-    
+    };
 };
 
+//Function to display message when library is empty.
 function displayMessage(){
     if(myLibrary.length === 0){
-        container.textContent="Your bookshelf is currently empty."
+        container.textContent = "Your bookshelf is currently empty."
     }
 };
-
 
 //Form event listener. Adds information to array on form submit.
 form.addEventListener("submit", function(e){
-    addNewBookToLibrary();
-    createBooks();
-    form.reset();
     e.preventDefault();
+    addNewBookToLibrary();
+    createBook();
+    hideModal();
+    form.reset();
 });
-
-//Add information to array and create example cards on load.
-document.addEventListener("DOMContentLoaded", addDefaultBooksToLibrary);
-document.addEventListener("DOMContentLoaded", createBooks);
 
 //Modal variables.
 let modal = document.getElementById("modal");
@@ -189,7 +189,6 @@ let closeBtn = document.getElementsByClassName("close")[0];
 //Function to show modal.
 function displayModal(){
     modal.style.display = "block";
-    getReadStatus();
 };
 
 //Function to hide modal.
